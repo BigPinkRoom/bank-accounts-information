6ca44fd624f23de3788fd6df31c2161fb767db57
Tester <TestTestTest1000@Test.com.ru>
2021-10-30 20:33:12 +0300

    add starting services layer

---
+++ services/operationsDays.js
@@ -0,0 +1,23 @@
+export const operationsDaysServices = {
+  get: {
+    convertData(response) {
+      const convertedData = response;
+      return convertedData;
+    },
+  },
+  add: {
+    createRequestBody(params) {
+      return params;
+    },
+  },
+  update: {
+    createRequestBody(params) {
+      return params;
+    },
+  },
+  delete: {
+    createRequestBody(params) {
+      return params;
+    },
+  },
+};
